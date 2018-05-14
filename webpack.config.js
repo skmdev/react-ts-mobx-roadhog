7476671967f38b0b3a1b7b87765bd2478a82fde0
sokaman <skmdev29@gmail.com>
2018-05-14 23:08:39 +0800

    Fix build bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const webpack = require('webpack');
 
-module.exports = function(webpackConfig, env) {
+module.exports = function(webpackConfig) {
+  // if (process.env.NODE_ENV === 'development') {
   webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
     if (!rule.use) return rule;
+
     rule.use = rule.use.map((loaderOption) => {
       if (loaderOption.loader && loaderOption.loader.indexOf('/css-loader/') > -1) {
-        return {
-          loader: require.resolve('typings-for-css-modules-loader'),
-          options: {
-            modules: true,
-            namedExport: true,
-            camelCase: true,
-            minimize: true,
-            localIdentName: '[local]_[hash:base64:5]',
-            ...loaderOption.options,
-          },
-        };
+        if (loaderOption.options.modules) {
+          return {
+            loader: require.resolve('typings-for-css-modules-loader'),
+            options: {
+              modules: true,
+              namedExport: true,
+              camelCase: true,
+              minimize: true,
+              localIdentName: '[local]_[hash:base64:5]',
+            },
+          };
+        }
+        return loaderOption;
       }
       if (typeof loaderOption === 'string' && loaderOption.indexOf('/style-loader/') > -1) {
         return {
@@ -26,16 +30,10 @@ module.exports = function(webpackConfig, env) {
     });
     return rule;
   });
-  webpackConfig.plugins.push(new webpack.WatchIgnorePlugin([/less\.d\.ts$/]));
-  // console.log(webpackConfig);
-  // sleep(11000000);
-  // function sleep(milliseconds) {
-  //   var start = new Date().getTime();
-  //   for (var i = 0; i < 1e7; i++) {
-  //     if (new Date().getTime() - start > milliseconds) {
-  //       break;
-  //     }
-  //   }
-  // }
+
+  if (process.env.NODE_ENV === 'development') {
+    webpackConfig.plugins.push(new webpack.WatchIgnorePlugin([/less\.d\.ts$/]));
+  }
+
   return webpackConfig;
 };
